d699fa0cd829c1cfbd3a5b0b09be25665a859ad2
Kai Ren <kai@phi.cc>
2026-09-08 14:46:03 +0800

    refactor: pass a dedicated highlight link URL for Canary

--- Sources/ChromiumBridge/ChromiumLauncher.m
+++ Sources/ChromiumBridge/ChromiumLauncher.m
@@ -141,9 +141,9 @@
 #if DEBUG || NIGHTLY_BUILD
                 [arguments addObject:@"--phi-ai-debug"];
                 [arguments addObject:@"--phi-no-embed-extensions"];
-                [arguments addObject:@"--phi-env=canary"];
-#else
-                [arguments addObject:@"--phi-env=release"];
+                // Chromium defaults to production; Canary uses the staging
+                // highlight-link endpoint to match its authentication setup.
+                [arguments addObject:@"--phi-highlight-link-url=https://stag.phi.to/api/v1/links"];
 #endif
 #if DEBUG
                 [arguments addObject:@"--no-sandbox"];
